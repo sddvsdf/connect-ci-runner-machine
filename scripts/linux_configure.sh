1105b15e8ba303f6d43403a07c7add7e05d02282
morio77 <howtostudyy77@gmail.com>
2021-06-12 20:16:13 +0900

    コメントをスリムに

--- scripts/linux_configure.sh
+++ scripts/linux_configure.sh
@@ -1,16 +1,8 @@
-#!/bin/bash
-# Reverse Linux VNC for GitHub Actions by PANCHO7532
-# This script is executed when GitHub actions is initialized.
-# Prepares dependencies, ngrok, and vnc stuff
-
 # First, install required packages...
 sudo apt update
 sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
 # Second, install TurboVNC
-# Fun Fact: TurboVNC is the only VNC implementations that supports OpenGL acceleration without an graphics device by default
-# By the way, you can still use the legacy version of this script where instead of installing TurboVNC, tightvncserver is installed.
-# Old mirror: wget https://ufpr.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
 wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
 sudo dpkg -i turbovnc_2.2.5_amd64.deb
 
