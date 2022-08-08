a35a66465afa9654fe533594033c8a3e4bbe6685
ifuad <i.fuad.tm@gmail.com>
2022-08-08 17:12:28 +0400

    ref

--- scripts/linux_configure.sh
+++ scripts/linux_configure.sh
@@ -3,8 +3,10 @@ sudo apt update
 sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
 # Second, install TurboVNC
-wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
-sudo dpkg -i turbovnc_2.2.5_amd64.deb
+#wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+#sudo dpkg -i turbovnc_2.2.5_amd64.deb
+
+sudo apt install -y tightvncserver
 
 # Third, download ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
@@ -12,7 +14,7 @@ unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 
 # Fourth, generate and copy passwd file and xstartup script
-export PATH=$PATH:/opt/TurboVNC/bin
+#export PATH=$PATH:/opt/TurboVNC/bin
 mkdir $HOME/.vnc
 cp ./resources/xstartup $HOME/.vnc/xstartup.turbovnc
 echo $VNC_USER_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
